caa3018aaa9f8c1439b2a341c7354db1b5f1113e
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2024-01-30 14:33:45 +0300

    Variables completed

--- Playgrounds/Variables.playground/Contents.swift
+++ Playgrounds/Variables.playground/Contents.swift
@@ -1,9 +1,40 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Variables & Constants
+
+//String
+
+var userName = "James"
+userName.append("o")
+
+userName.lowercased()
+userName.uppercased()
+
+userName = "Hans"
+
+//integer & double & float
+
+//integer
+let userAge = 50
 
 
-var number = 5 * 4
-print(number)
+//double
+let userAgeD = 50.0
+let myNumberD = 4.0
+
+userAgeD / myNumberD
+
+//boolean
+var myBoolean = true
 
 
+var myString : String = "50"
+let anotherNumber : Int = Int(10.5)
+
+let stringNumber : String = String(20)
+
+//define
+
+let myVariable : String //
+
+//initialization
