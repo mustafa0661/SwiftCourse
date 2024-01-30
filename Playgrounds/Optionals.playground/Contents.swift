caff2094c693667ce16f1396833f94034c94e794
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2024-01-30 15:59:11 +0300

    optionals completed

---
+++ Playgrounds/Optionals.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var myName : String?
+
+myName?.uppercased()
+
+//optionals : ? vs !
+
+var myAge = "f"
+
+var myInteger = (Int(myAge) ?? 0) * 5
+
+if let myNumber = Int(myAge){
+    print(myNumber * 5)
+} else{
+    print("wrong input")
+}
+    
