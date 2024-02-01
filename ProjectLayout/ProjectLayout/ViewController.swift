0c0539b2f6bba75916ce9eee21c9a0fec41aea45
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2024-02-01 19:01:53 +0300

    Layout project completed

---
+++ ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Mustafa UZUN on 1.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
